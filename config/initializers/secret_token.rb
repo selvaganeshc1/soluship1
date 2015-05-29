a311b6c92c335e4b9aebb949b94fa6d4ef1c163c
selvaganeshc1 <selvaganesh.c@justintegrateit.com>
2015-05-29 15:36:33 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Soluship1::Application.config.secret_key_base = '2b7b8fba9fe7de8e06628361021a6adcff209fb3f259132e2977a719cb1549200c8773eea8c2eff7a8e2f3f5f7f2f0397d172f34de1a6472e4bea3980570f876'
